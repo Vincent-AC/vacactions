008ea5b471d9f20b8b1f885fef1312531e37c537
Vincent Aranzana-Climent <vincent.climent@farmbio.uu.se>
2020-09-21 15:41:57 +0200

    Updated documentation Added multiple input to function Added overwrite argument

--- R/create_updated_modfiles.R
+++ R/create_updated_modfiles.R
@@ -1,7 +1,47 @@
-create_updated_modfiles <- function(input_model_files, output_model_files)
-{
-  pharmpy <- reticulate::import("pharmpy")
-  model <- pharmpy$Model(input_model_files)
-  if (file.exists(gsub(".mod",".ext",input_model_files))) model$update_inits()
-  model$write(output_model_files)
-}
+#' Create multiple NONMEM model files with updated initial estimates
+#'
+#' The point of this function is to enable creation of multiple NONMEM model
+#' files, with updated initial estimates when possible. It is possible to copy a
+#' single model file multiple times or to create multiple output model files
+#' from multiple input model files.
+#'
+#' @param input_model_files character vector of length 1 or n equal to the
+#' number of output files. Contains path(s) to model(s) to be copied with updated
+#' parameter initial estimates.
+#' @param output_model_files character vector of length n. Contains path(s) to
+#' new model files.
+#' @param overwrite boolean of length 1. If TRUE and output_model_files refers
+#' to an already existing file, the existing file will be overwritten.
+#' Defaults to TRUE.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{create_updated_modfiles("run1.mod",c("run2.mod","run3.mod"))}
+create_updated_modfiles <-
+  function(input_model_files,
+           output_model_files,
+           overwrite = TRUE)
+  {
+    pharmpy <- reticulate::import("pharmpy")
+
+    update_single_modfile <-
+      function(input_model_file,
+               output_model_file,
+               overwrite)
+      {
+        model <- pharmpy$Model(input_model_file)
+
+        if (file.exists(gsub(".mod", ".ext", input_model_file)))
+          model$update_inits()
+
+        model$write(output_model_file, force = overwrite)
+      }
+
+    purr::walk2(
+      input_model_files,
+      output_model_files,
+      update_single_modfile(overwrite = overwrite)
+    )
+  }
