c280550cf05ddab69d5d67af78cc1c06deb78c15
Vincent Aranzana-Climent <vincent.climent@farmbio.uu.se>
2020-09-22 10:04:59 +0200

    Added dir argument to create_updated_modfiles

--- R/create_updated_modfiles.R
+++ R/create_updated_modfiles.R
@@ -5,11 +5,15 @@
 #' single model file multiple times or to create multiple output model files
 #' from multiple input model files.
 #'
+#' @param dir_input_files character. Contains path to directory containing the
+#' input model files. Defaults to "".
 #' @param input_model_files character vector of length 1 or n equal to the
-#' number of output files. Contains path(s) to model(s) to be copied with updated
+#' number of output files. Contains name(s) of model(s) to be copied with updated
 #' parameter initial estimates.
-#' @param output_model_files character vector of length n. Contains path(s) to
-#' new model files.
+#' @param dir_output_files character. Contains path to directory containing the
+#' output model files. Defaults to "".
+#' @param output_model_files character vector of length n. Contains name(s) of
+#' new model file(s).
 #' @param overwrite boolean of length 1. If TRUE and output_model_files refers
 #' to an already existing file, the existing file will be overwritten.
 #' Defaults to TRUE.
@@ -20,28 +24,36 @@
 #' @examples
 #' \dontrun{create_updated_modfiles("run1.mod",c("run2.mod","run3.mod"))}
 create_updated_modfiles <-
-  function(input_model_files,
+  function(dir_input_files="",
+           input_model_files,
+           dir_output_files="",
            output_model_files,
            overwrite = TRUE)
   {
     pharmpy <- reticulate::import("pharmpy")
 
     update_single_modfile <-
-      function(input_model_file,
+      function(dir_input_file,
+               input_model_file,
+               dir_output_file,
                output_model_file,
                overwrite)
       {
-        model <- pharmpy$Model(input_model_file)
+        model <- pharmpy$Model(base::paste0(dir_input_file, input_model_file))
 
-        if (file.exists(gsub(".mod", ".ext", input_model_file)))
+        if (file.exists(base::paste0(dir_input_file, gsub(".mod", ".ext", input_model_file))))
           model$update_inits()
 
-        model$write(output_model_file, force = overwrite)
+        model$write(base::paste0(dir_output_file, output_model_file), force = overwrite)
       }
 
     purrr::walk2(
       input_model_files,
       output_model_files,
-      update_single_modfile(overwrite = overwrite)
+      update_single_modfile(
+        dir_input_files = dir_input_files,
+        dir_output_files = dir_output_files,
+        overwrite = overwrite
+      )
     )
   }
