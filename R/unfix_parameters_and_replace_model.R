c4f69c051a31bf961de5fe7e0f42dbad4dbaa68d
Vincent Aranzana-Climent <vincent.climent@farmbio.uu.se>
2020-11-23 10:19:00 +0100

    Updated unfix_parameters_and_replace_model function for it to work with multiple target models

--- R/unfix_parameters_and_replace_model.R
+++ R/unfix_parameters_and_replace_model.R
@@ -3,8 +3,7 @@
 #' What the title says. A model file and a character vector of parameters to be
 #' unfixed has to be provided
 #'
-#' @param dir_input_files character of length 1 or n equal to the
-#' number of input files. Contains path to directory containing the
+#' @param dir_input_files character of length 1. Contains path to directory containing the
 #' input model files. Defaults to "".
 #' @param input_model_files character vector of length 1 or n equal to the
 #' number of input files. Contains name(s) of model(s) to be changed with some
@@ -27,12 +26,31 @@ unfix_parameters_and_replace_model <- function(dir_input_files,
   fix_parameter <- modeling$fix_parameters
   unfix_parameter <- modeling$unfix_parameters
 
-  model <- pharmpy$Model(paste0(dir_input_files,input_model_files))
-  purrr::walk(parameters_to_unfix,~unfix_parameter(model,.x))
-  purrr::walk(parameters_to_unfix,~usethis::ui_info((base::paste0("Unfixed ",.x))))
-  modeling$update_source(model)
-  model$write(paste0(dir_input_files,input_model_files),
-              force = T)
-  usethis::ui_info(base::paste0("Update of ", input_model_files,
-                                " done"))
+  single_model_function <- function(dir_input_file,
+                                    input_model_file,
+                                    parameters_to_unfix_single)
+  {
+    model <- pharmpy$Model(paste0(dir_input_file, input_model_file))
+    purrr::walk(parameters_to_unfix_single,  ~ unfix_parameter(model, .x))
+    purrr::walk(parameters_to_unfix_single,  ~ usethis::ui_info((base::paste0("Unfixed ", .x))))
+    modeling$update_source(model)
+    model$write(paste0(dir_input_file, input_model_file),
+                force = T)
+    usethis::ui_info(base::paste0("Update of ", input_model_file,
+                                  " done"))
+  }
+
+  purrr::walk2(
+    input_model_files,
+    parameters_to_unfix,
+    purrr::possibly(
+      ~ single_model_function(
+        dir_input_file = dir_input_files,
+        input_model_file = .x,
+        parameters_to_unfix_single = .y
+      ), otherwise = NULL, quiet = F
+    )
+  )
+
+
 }
